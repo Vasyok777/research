bce74f58f15daf51f90a6f0bd3a3a6864e5a11c1
Vasyl Holub <107894803+Vasyok777@users.noreply.github.com>
2023-11-14 11:26:04 +0200

    Add files via upload

--- js/author.js
+++ js/author.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const buttons = document.querySelectorAll(
-		'.author-info__bottom-buttons button'
-	)
-	const icons = document.querySelectorAll('.icons__item')
-	const iconsText = document.querySelector('.icons__text')
-	const hiddenButtons = document.querySelector('.hidden-buttons')
+// document.addEventListener('DOMContentLoaded', function () {
+// 	const buttons = document.querySelectorAll(
+// 		'.author-info__bottom-buttons button'
+// 	)
+// 	const icons = document.querySelectorAll('.icons__item')
+// 	const iconsText = document.querySelector('.icons__text')
+// 	const hiddenButtons = document.querySelector('.hidden-buttons')
 
-	if (window.innerWidth <= 900) {
-		buttons.forEach((button, index) => {
-			if (index >= 3) {
-				button.style.display = 'none'
-			}
-		})
-		if (window.innerWidth <= 900) {
-			icons.forEach((icon, index) => {
-				if (index >= 6) {
-					icon.style.display = 'none'
-				}
-			})
-		}
+// 	if (window.innerWidth <= 900) {
+// 		buttons.forEach((button, index) => {
+// 			if (index >= 3) {
+// 				button.style.display = 'none'
+// 			}
+// 		})
+// 		if (window.innerWidth <= 900) {
+// 			icons.forEach((icon, index) => {
+// 				if (index >= 6) {
+// 					icon.style.display = 'none'
+// 				}
+// 			})
+// 		}
 
-		hiddenButtons.textContent = `+${buttons.length + 2} more`
-		iconsText.textContent = `+${icons.length + 7} more`
-	}
+// 		hiddenButtons.textContent = `+${buttons.length + 2} more`
+// 		iconsText.textContent = `+${icons.length + 7} more`
+// 	}
 })
